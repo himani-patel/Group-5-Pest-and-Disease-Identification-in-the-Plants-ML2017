e5ce4715786b204a35236ccc85e1215c1796fbe6
RupamJogal <rupam.j.btechi14@ahduni.edu.in>
2017-05-09 23:04:35 +0530

    Delete predictModel.m

--- Final_Submission/predictModel.m
+++
@@ -1,8 +0,0 @@
-
-%function [Class,index_image] = predictModel
-  load Model_white.mat
-  load Matrix_test_white.mat
-  %[matrix_Test,index_image]=LearnTest();
-  Class =predict(training,mat);
-%end
-
