0b011a4596fe0128a3efc3b70848bd7cd00fd581
divya-patel11 <divya.patel110297@gmail.com>
2017-05-10 08:25:25 +0530

    Add files via upload

--- Final_Submission/Code/predictModel.m
+++ Final_Submission/Code/predictModel.m
@@ -1,8 +1,12 @@
 
+
+
 %function [Class,index_image] = predictModel
   load Model_white.mat
   load Matrix_test_white.mat
   %[matrix_Test,index_image]=LearnTest();
-  Class =predict(training,mat);
+  Class =predict(training_white,mat);
+  
+  save('Class','Class')
 %end
 
---
+++ Final_Submission/Code/segment.m
@@ -0,0 +1,45 @@
+clear all
+close all
+
+load Class.mat
+load index_image.mat
+
+folderName='/media/divya/EDUCATION/SEAS_6th_Sem/ML/Final_ML/Test/';
+  extension='*.jpg';
+  concattedString=strcat(folderName, extension);
+  fileSet=dir(concattedString); 
+  index=find(Class(:)==0);
+  cellSize=35;
+  applyMat = zeros(cellSize,cellSize,3);
+  l=1;
+  for im = 1:length(fileSet)
+    im
+    I=imread(strcat(folderName, fileSet(im).name));  
+    figure,imshow(I);
+    [r,c,d]=size(I);
+    for k=1:cellSize
+      if(mod((r+k),cellSize)==0)
+        row=r-(cellSize-k);
+      end
+      if(mod((c+k),cellSize)==0)
+        col=c-(cellSize-k);
+      end
+    end
+    I(:,col:c,:)=0;
+    I(row:r,:,:)=0;
+    for j =(round(cellSize/2)):cellSize:row
+        for k = (round(cellSize/2)):cellSize:col
+          if(Class(l)==1)
+            I(j-(round(cellSize/2)-1):j+(round(cellSize/2)-1),k-(round(cellSize/2)-1):k+(round(cellSize/2)-1),1:3) = applyMat;            
+          end
+          l=l+1;
+        end
+    end
+    figure,imshow(I);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  [Out,disease_severity] = classify2(I);
+    figure,imshow(Out);
+    severity(im) = disease_severity;
+  end
+%end
